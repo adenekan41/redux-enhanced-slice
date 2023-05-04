a5b32a35c1c613f13133b621d13fab09b1cb5f2c
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2023-05-04 02:58:12 +0100

    refactor(types.ts): add new types for `IgnoreDefaultReducers`, `SliceConsolidateReducers`, `RootState`, and `ApiEndpointQuery`

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,25 @@
+import type { ActionCreatorWithOptionalPayload } from '@reduxjs/toolkit';
+
 export interface SliceState<T> {
   results: T;
-  isLoading: boolean;
+  isLoading?: boolean;
   page?: number;
   hasMore?: boolean;
   errors?: any;
 }
-
+export type NoInfer<T> = [T][T extends any ? 0 : never];
 export type ObjectKeys<T extends unknown> = keyof T;
-export type InitialStateSlice<T extends unknown, S extends string> = {
-  [K in ObjectKeys<T> as SliceString<S, K>]: SliceState<T[K]>;
+export type IgnoreDefaultReducers<
+  T extends unknown,
+  J extends unknown,
+  Z
+> = Z['ignoreDefaultReducers' & keyof Z] extends true ? J : T;
+export type InitialStateSlice<T, S extends string, Z> = {
+  [K in ObjectKeys<T> as SliceString<S, K>]: IgnoreDefaultReducers<
+    SliceState<T[K]>,
+    T[K],
+    Z
+  >;
 };
 
 /* ------------------------------- Slice Types ------------------------------ */
@@ -21,9 +32,16 @@ export type SliceSelector<
 export type SliceEnum<K, S extends string> = Readonly<{
   [U in ObjectKeys<K>]: SliceString<S, U>;
 }>;
+/**
+ * Because we are using the `createSlice` function from `@reduxjs/toolkit`, we need to define the `CaseReducer` type.
+ * and map it to the actions, not the best solution but it works.
+ */
+export type SliceConsolidateReducers<Z extends unknown, S extends string> = {
+  [U in keyof Z]: ActionCreatorWithOptionalPayload<
+    Parameters<Z[U] extends (...args: any) => any ? Z[U] : never>[1]['payload'],
+    `${S}/${U & string}`
+  >;
+};
 
-export type ReturnType<T extends (...args: any) => any> = T extends (
-  ...args: any
-) => infer R
-  ? R
-  : any;
+export type RootState = ReturnType<any>;
+export type ApiEndpointQuery<T, S> = any[];
